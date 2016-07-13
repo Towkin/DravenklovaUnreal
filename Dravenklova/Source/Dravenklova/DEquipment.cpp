be4c914aeba18914488db510f4488d5191677656
Emanuel Strömgren <emanuel.stromgren@pulpeten.se>
2016-07-13 16:43:17 +0200

    Updated DCharacter and DEquipment for better Blueprints workflow. Added some new meshes for lantern and player.

--- Dravenklova/Source/Dravenklova/DEquipment.cpp
+++ Dravenklova/Source/Dravenklova/DEquipment.cpp
@@ -8,13 +8,16 @@
 
 void ADEquipment::Equip(ADCharacter* pawn)
 {
+	UE_LOG(LogTemp, Warning, TEXT("Equipping..."));
+
 	switch (m_Slot)
 	{
 	case EEquipmentSlot::Primary:
 		if (!pawn->GetPrimary())
 		{
+			ReceiveEquipped(pawn, m_Slot);
 			pawn->SetPrimary(this);
-			ReceiveEquip(pawn, m_Slot);
+			UE_LOG(LogTemp, Warning, TEXT("Equipped to Primary"));
 		}
 		else
 		{
@@ -24,8 +27,9 @@ void ADEquipment::Equip(ADCharacter* pawn)
 	case EEquipmentSlot::Secondary:
 		if (!pawn->GetSecondary())
 		{
+			ReceiveEquipped(pawn, m_Slot);
 			pawn->SetSecondary(this);
-			ReceiveEquip(pawn, m_Slot);
+			UE_LOG(LogTemp, Warning, TEXT("Equipped to Secondary"));
 		}
 		else
 		{
@@ -35,39 +39,47 @@ void ADEquipment::Equip(ADCharacter* pawn)
 		break;
 
 	case EEquipmentSlot::Sampler:
+		ReceiveEquipped(pawn, m_Slot);
 		pawn->SetSampler(this);
-		ReceiveEquip(pawn, m_Slot);
 		break;
 
 	default:
 		break;
 	}
 
-	UE_LOG(LogTemp, Warning, TEXT("Equip"));
-}
-
-
-void ADEquipment::UnequipPrimary(ADCharacter* pawn)
-{
-	UE_LOG(LogTemp, Warning, TEXT("Unequip primary"));
 	
-	ReceiveUnequipPrimary(pawn);
+}
 
-}
-void ADEquipment::UnequipSecondary(ADCharacter* pawn)
+void ADEquipment::Unequip(ADCharacter* pawn)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Unequip secondary"));
-	
-	ReceiveUnequipSecondary(pawn);
+	UE_LOG(LogTemp, Warning, TEXT("Unequipped"));
+
+	ReceiveUnequipped(pawn);
 }
+
+//void ADEquipment::UnequipPrimary(ADCharacter* pawn)
+//{
+//	UE_LOG(LogTemp, Warning, TEXT("Unequip primary"));
+//	
+//	ReceiveUnequipPrimary(pawn);
+//
+//}
+//void ADEquipment::UnequipSecondary(ADCharacter* pawn)
+//{
+//	UE_LOG(LogTemp, Warning, TEXT("Unequip secondary"));
+//	
+//	ReceiveUnequipSecondary(pawn);
+//}
 
  void ADEquipment::Interact(ADCharacter* pawn)
 {
+	Super::Interact(pawn);
+
 	Equip(pawn);
 }
 
 void ADEquipment::EndInteract(ADCharacter* pawn)
 {
-	
+	Super::EndInteract(pawn);
 }
 
